567226617e76b1d9baf86743a1d2fe99a69e194c
Oracy Martos <oramartos_21@hotmail.com>
2019-12-05 17:04:52 -0300

    PSQL Created

--- Scripts/replication.sql
+++ Scripts/replication.sql
@@ -1,1 +1,3 @@
-CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replication';
+CREATE USER repuser WITH REPLICATION ENCRYPTED PASSWORD 'replication';
+archive_command = 'test ! -f /var/lib/postgresql/data/mnt/server/archivedir/%f && cp %p /var/lib/postgresql/data/mnt/server/archivedir/%f'
+repuser - v - P --xlog-method=stream
